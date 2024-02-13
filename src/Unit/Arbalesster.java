d0f4a2cafdb57242b2669211e35a257a6e38fb6a
gulnazgentle-kazan <gulnazvp@mail.ru>
2024-02-13 23:37:54 +0300

    created arbalesster

---
+++ src/Unit/Arbalesster.java
@@ -0,0 +1,7 @@
+package Unit;
+
+public class Arbalesster extends BaseUnit{
+    public Arbalesster(String name, String typeDamage, int hp, int maxhp, int armor, int speedMove, int damage, int speedDamage, int rangeDamage) {
+        super("Arbaletchik", "arbalet", 100, 100, 5, 5, 5, 10, 5);
+    }
+}
