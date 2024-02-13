1bd957bdcbf5756efbe58b26c3a5d018402a716e
gulnazgentle-kazan <gulnazvp@mail.ru>
2024-02-13 23:40:53 +0300

    created monk

---
+++ src/Unit/Monk.java
@@ -0,0 +1,7 @@
+package Unit;
+
+public class Monk extends BaseUnit{
+    public Monk(String name, String typeDamage, int hp, int maxhp, int armor, int speedMove, int damage, int speedDamage, int rangeDamage) {
+        super("Monakh", "prayer", 100, 100, 1, 1, 0, 0, 0);
+    }
+}
