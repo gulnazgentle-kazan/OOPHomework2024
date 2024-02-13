1da364232f38007ea0739271f7d5b7a1082df184
gulnazgentle-kazan <gulnazvp@mail.ru>
2024-02-13 23:15:53 +0300

    created outlaw

---
+++ src/Unit/Outlaw.java
@@ -0,0 +1,7 @@
+package Unit;
+
+public class Outlaw extends BaseUnit{
+    Outlaw(String name, String typeDamage, int hp, int maxhp, int armor, int speedMove, int damage, int speedDamage, int range){
+        super("Solovey", "gun", 100, 100, 10, 10, 10, 10, 10);
+    }
+}
