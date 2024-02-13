abb93b8f5cd0d0ad701aaca669c741b502149132
gulnazgentle-kazan <gulnazvp@mail.ru>
2024-02-13 23:09:05 +0300

    created Peasant

---
+++ src/Unit/Peasant.java
@@ -0,0 +1,7 @@
+package Unit;
+
+public class Peasant extends BaseUnit {
+    public Peasant(String name, String typeDamage, int hp, int maxhp, int armor, int speedMove, int damage, int speedDamage, int rangeDamage) {
+        super("Ivan", "pitchfork", 100, 100, 0, 1, 1, 1, 1);
+    }
+}
