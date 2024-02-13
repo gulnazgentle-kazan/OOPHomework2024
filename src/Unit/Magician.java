83dfb0d8115b1ecfebf89cbdb8f0c9254ab7a66e
gulnazgentle-kazan <gulnazvp@mail.ru>
2024-02-13 23:31:51 +0300

    created magician

---
+++ src/Unit/Magician.java
@@ -0,0 +1,7 @@
+package Unit;
+
+public class Magician extends BaseUnit{
+    public Magician(String name, String typeDamage, int hp, int maxhp, int armor, int speedMove, int damage, int speedDamage, int rangeDamage) {
+        super("Koldun", "magic", 100, 100, 5, 5, 5, 5, 5);
+    }
+}
