67bc71f4696eda8b8990a0cf479b6e00e5c7b0b6
gulnazgentle-kazan <gulnazvp@mail.ru>
2024-02-13 23:34:48 +0300

    created spearman

---
+++ src/Unit/Spearman.java
@@ -0,0 +1,7 @@
+package Unit;
+
+public class Spearman extends BaseUnit{
+    public Spearman(String name, String typeDamage, int hp, int maxhp, int armor, int speedMove, int damage, int speedDamage, int rangeDamage) {
+        super("Kopeyshchik", "spear", 100, 100, 10, 10, 5, 5, 5);
+    }
+}
