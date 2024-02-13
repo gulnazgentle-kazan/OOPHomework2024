411376c7668559e15802a2b3b0689ccf45c96d9b
gulnazgentle-kazan <gulnazvp@mail.ru>
2024-02-13 23:28:04 +0300

    created sniper

---
+++ src/Unit/Sniper.java
@@ -0,0 +1,7 @@
+package Unit;
+
+public class Sniper extends BaseUnit{
+    public Sniper(String name, String typeDamage, int hp, int maxhp, int armor, int speedMove, int damage, int speedDamage, int rangeDamage) {
+        super("Mozilla", "rifle", 100, 100, 10, 1, 10, 10, 10);
+    }
+}
