f7f830995c73bb27211ccf05b7777d7a0ce35131
gulnazgentle-kazan <gulnazvp@mail.ru>
2024-02-14 22:38:38 +0300

    created object in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import Unit.*;
+
+import java.lang.foreign.SymbolLookup;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Arbalesster arbalesster = new Arbalesster("Arbaletchik", "arbalet", 100, 100, 5, 5, 5, 10, 5);
+        Magician magician = new Magician("Koldun", "magic", 100, 100, 5, 5, 5, 5, 5);
+        Monk monk = new Monk("Monakh", "prayer", 100, 100, 1, 1, 0, 0, 0);
+        Outlaw outlaw = new Outlaw("Solovey", "gun", 100, 100, 10, 10, 10, 10, 10);
+        Peasant peasant = new Peasant("Ivan", "pitchfork", 100, 100, 0, 1, 1, 1, 1);
+        Sniper sniper = new Sniper("Mozilla", "rifle", 100, 100, 10, 1, 10, 10, 1);
+        Spearman spearman = new Spearman("Kopeyshchik", "spear", 100, 100, 10, 10, 5, 5, 5);
+
     }
 }
--- src/Unit/Outlaw.java
+++ src/Unit/Outlaw.java
@@ -1,9 +1,11 @@
 package Unit;
 
-public class Outlaw extends BaseUnit{
-    Outlaw(String name, String typeDamage, int hp, int maxhp, int armor, int speedMove, int damage, int speedDamage, int range){
+public class Outlaw extends BaseUnit {
+    public Outlaw(String name, String typeDamage, int hp, int maxhp, int armor, int speedMove, int damage,
+            int speedDamage, int range) {
         super("Solovey", "gun", 100, 100, 10, 10, 10, 10, 10);
     }
+
     @Override
     public String toString() {
         return "Соловей";
